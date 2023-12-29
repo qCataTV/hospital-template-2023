bce9d96424a264c4216d07f108de43eea86a1e7f
qCata <75864270+qCataTV@users.noreply.github.com>
2023-12-29 12:52:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript to add scrolled class to body on scroll
+window.addEventListener('scroll', function() {
+    const body = document.querySelector('body');
+    const scrolled = window.scrollY > 50; // Change 50 to the desired scroll position
+
+    if (scrolled) {
+        body.classList.add('scrolled');
+    } else {
+        body.classList.remove('scrolled');
+    }
+});
+
+var myname = "Catalin";
